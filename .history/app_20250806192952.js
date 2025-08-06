fb6b5874aec8f9966d7d5f683190e19c47b60a4e
kunal hande <kunalhand30@gmail.com>
2025-08-06 20:11:51 +0530

    first commit

---
+++ .history/app_20250806192952.js
@@ -0,0 +1,18 @@
+ const express = require("express");
+ const app = express();
+
+ app.use(() =>{
+    console.log("hi, i am middleware");
+ });
+
+app.get("/", (req,res) =>{
+    res.send("history, i am root");
+});
+
+app.get("/random", (req,res) =>{
+    res.send("This is a random page");
+})
+
+ app.listen(8080,()=>{
+    console.log("server is listening on port 8080");
+ });
